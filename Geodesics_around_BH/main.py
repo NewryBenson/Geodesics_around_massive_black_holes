e6b696a1b80b4215d6e979f5ca3f669e9d571e90
Newry <sander.vandeneynden@hotmail.com>
2023-10-31 10:42:28 +0100

    fotos

--- Geodesics_around_BH/main.py
+++ Geodesics_around_BH/main.py
@@ -8,6 +8,18 @@ import multiprocess.pool as Pool
 import matplotlib.pyplot as plt
 import scipy
 
+class Background:
+    def __init__(self, image: Image):
+        self.image = image
+
+    def getPixelValue(self, pixely, pixelx):
+        return self.image.load()[pixelx, pixely]
+
+    def getAngleValue(self, theta, phi):
+        return self.getPixelValue(math.floor(theta/np.pi*self.image.size[1]), math.floor(phi/(2*np.pi)*self.image.size[0]))
+
+
+
 #black hole class
 class BlackHole:
     def __init__(self,x:float,y:float, z:float, mass: float):
@@ -54,16 +66,19 @@ class Camera:
 class Ray:
     #pixelx and pixely is the pixel location on the screen of the pixel this ray passes through
     #camera is the camera object the ray comes from
-    def __init__(self, pixely: int, pixelz: int, camera: Camera):
+    def __init__(self, pixely: int, pixelz: int, camera: Camera, background: Background):
         self.pixelz = pixelz
         self.pixely = pixely
         self.camera = camera
+        self.background = background
         self.z = -2*pixelz*camera.size/camera.resolution + camera.size - self.camera.size/(2*self.camera.resolution)
         self.y = -2*pixely*camera.size/camera.resolution + camera.size - self.camera.size/(2*self.camera.resolution)
         self.x = camera.distance
         self.r = np.sqrt(self.x**2 + self.y**2 + self.z**2)
-        self.phi = np.arccos(self.z/self.r)
-        self.theta = -np.sign(self.y)*np.arccos(self.x/np.sqrt(self.x**2 + self.y**2))+np.pi
+        self.theta = np.arccos(self.z/self.r)
+        self.phi = np.arctan(self.y/self.x)+np.pi
+        #self.phi = -np.sign(self.y)*np.arccos(self.x/np.sqrt(self.x**2 + self.y**2))+np.pi
+
 
 
     #returns the pixel location on the screen
@@ -79,15 +94,26 @@ class Ray:
         return (self.r, self.theta, self.phi)
 
     def getColor(self) -> (int, int, int):
-        return (math.floor(self.phi/np.pi*255), 0, math.floor(self.theta/(2*np.pi) *255))
+        return self.background.getAngleValue(self.theta, self.phi)
+
+    def getColorGradient(self) -> (int, int, int):
+        return (math.floor(self.theta / np.pi * 255), 0, math.floor(self.phi / (2 * np.pi) * 255))
+
+
+
 
 
 #Define all differential equations (coupled)
 def dSdl(l, S, M):
     t, r, theta, phi, pt, pr, ptheta, pphi = S
-    return [-1/(1-(2*M/r))*pt, (1-(2*M/r))*pr, (r**-2)*ptheta, (r**-2)*pphi,
-            0, -(1/2)*( (1-(2*M/r))**-2 * (2*M/(r**2))*pt**2 + (2*M/(r**2))*pr**2 -2*(r**-3)*ptheta**2 -2*(r**-3)*np.sin(theta)**2 * pphi**2),
-            np.cos(theta)*(np.sin(theta))**-3 * r**-2 * pphi**2, 0]
+    return [-1/(1-(2*M/r))*pt,
+            (1-(2*M/r))*pr,
+            (r**-2)*ptheta,
+            (r**-2)*pphi,
+            0,
+            -(1/2)*( (1-(2*M/r))**-2 * (2*M/(r**2))*pt**2 + (2*M/(r**2))*pr**2 -2*(r**-3)*ptheta**2 -2*(r**-3)*np.sin(theta)**2 * pphi**2),
+            np.cos(theta)*(np.sin(theta))**-3 * r**-2 * pphi**2,
+            0]
 
 #l_span should be an (l0, lfinal) object
 def solver(t0, r0, theta0, phi0, pt0, pr0, ptheta0, pphi0, l_span):
@@ -99,17 +125,18 @@ def solver(t0, r0, theta0, phi0, pt0, pr0, ptheta0, pphi0, l_span):
 
 def main():
     black_hole = BlackHole(0, 0, 10, 1)
-    camera = Camera(0.1, 400, 1)
+    camera = Camera(3, 400, 1)
     screen = Image.new(mode="RGB", size=(camera.getResolution(), camera.getResolution()))
+    background = Background(Image.open("ColorGrid.png"))
     rays={}
     screenPixels = screen.load()
-    for y in range(camera.getResolution()):
-        for z in range(camera.getResolution()):
-            rays[(y, z)] = Ray(y, z, camera)
+    for x in range(camera.getResolution()):
+        for y in range(camera.getResolution()):
+            rays[(x, y)] = Ray(x, y, camera, background)
 
-    for y in range(camera.getResolution()):
-        for z in range(camera.getResolution()):
-            screenPixels[y, z] = rays[(y, z)].getColor()
+    for x in range(camera.getResolution()):
+        for y in range(camera.getResolution()):
+            screenPixels[x, y] = rays[(x, y)].getColor()
 
     screen.show()
 
